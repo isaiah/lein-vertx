25d3a23bbdd40f510bef24fc853f8aef11b905d2
isaiah <issaria@gmail.com>
2013-10-04 10:33:25 +0200

    update sample project dependencies

--- sample-app/project.clj
+++ sample-app/project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.vertx/vertx-core "2.0.1-final"]
+                 [io.vertx/vertx-platform "2.0.1-final"]
+                 ]
+  :plugins [[lein-vertx "0.1.0-SNAPSHOT"]]
   :vertx {:main-fn sample-app.core/init})
