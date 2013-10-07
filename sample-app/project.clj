7075babacc2190377003c2938dea6e7cae71e2e4
Toby Crawley <toby@tcrawley.org>
2013-10-06 22:19:48 -0400

    Add io.vertx/clojure-api to provided deps so lein check works.

--- sample-app/project.clj
+++ sample-app/project.clj
@@ -3,9 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.vertx/vertx-core "2.0.1-final"]
-                 [io.vertx/vertx-platform "2.0.1-final"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-vertx "0.1.0-SNAPSHOT"]]
+  :profiles {:provided
+             {:dependencies [[io.vertx/clojure-api "0.2.0"]]}}
   :vertx {:main-fn sample-app.core/init})
