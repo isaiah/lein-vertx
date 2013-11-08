60a70fe9fe57ce04c9c4f6ac91569113d26c6ee5
Toby Crawley <toby@tcrawley.org>
2013-11-08 15:25:57 -0500

    Update to Vert.x 2.1M1 and lang-clojure 0.3.0.

--- src/lein_vertx/core.clj
+++ src/lein_vertx/core.clj
@@ -41,7 +41,7 @@
 
 (def ^:internal vertx-deps-project
   "Dummy project used to resolve vertx deps for the classpath of the subprocess"
-  {:dependencies '[[io.vertx/vertx-platform "2.0.1-final"]]
+  {:dependencies '[[io.vertx/vertx-platform "2.1M1"]]
    :repositories [["central" {:url "http://repo1.maven.org/maven2/" :snapshots false}]
                   ["sonatype" {:url "http://oss.sonatype.org/content/repositories/snapshots" :snapshots true}]
                   ["bintray" {:url "http://dl.bintray.com"}]]})
